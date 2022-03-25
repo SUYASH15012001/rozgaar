e4f2e4b596afa72185df94c04bfa77228ade898e
shogun website <shogun@simplifii.com>
2022-03-25 14:22:36 +0530

    piechart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import "./routes/Analytics";
+import Analytics from "./routes/Analytics";
 
 export default function App() {
   return (
@@ -8,7 +10,7 @@ export default function App() {
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<Analytics />} />
       </Routes>
     </div>
   );
@@ -27,16 +29,16 @@ function Home() {
   );
 }
 
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
+// function About() {
+//   return (
+//     <>
+//       <main>
+//         <h2>Who are we?</h2>
+//         <p>That feels like an existential question, don't you think?</p>
+//       </main>
+//       <nav>
+//         <Link to="/">Home</Link>
+//       </nav>
+//     </>
+//   );
+// }
